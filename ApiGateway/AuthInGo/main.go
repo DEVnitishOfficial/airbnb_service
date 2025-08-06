4740a2d3e1b52c244496660182bd350f80d26d1e
DEVnitishOfficial <nitish.naroun123@gmail.com>
2025-08-07 00:09:22 +0530

    refracotor code by adding constructor

--- ApiGateway/AuthInGo/main.go
+++ ApiGateway/AuthInGo/main.go
@@ -7,15 +7,10 @@ import (
 func main() {
 
 	// creating a config object
-	// this config object will be used to configure the server
-	cfg := app.Config{
-		Addr: ":3005", // specify the port
-	}
-	// the run() function is available on the Application struct
-	// so we have to create an object of Application struct
-	app := app.Application{
-		Config: cfg,
-	}
+	cfg := app.NewConfig(":3005")
+
+	// created an object of Application struct
+	app := app.NewApplication(cfg)
 
 	app.Run()
 }
