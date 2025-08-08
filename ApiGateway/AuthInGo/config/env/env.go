8bdc9c09e16955c5bcb0ecbe015425094af4aba5
DEVnitishOfficial <nitish.naroun123@gmail.com>
2025-08-08 12:15:38 +0530

    get custom value from .env file  like string, int, bool

--- ApiGateway/AuthInGo/config/env/env.go
+++ ApiGateway/AuthInGo/config/env/env.go
@@ -3,11 +3,12 @@ package config
 import (
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
 )
 
-func load() {
+func Load() {
 	err := godotenv.Load()
 	if err != nil {
 		// log the error if .env file is not found
@@ -20,10 +21,41 @@ func load() {
 // we just call the GetString function with key and we get the value
 // if the key is not found, it returns the fallback value
 func GetString(key string, fallback string) string {
-	load() // ensure .env is loaded before accessing any keys
 	value, ok := os.LookupEnv(key)
 	if !ok {
 		return fallback
 	}
 	return value
+}
+
+// from env we always get string but suppose we want to get int then we can use below function
+func GetInt(key string, fallback int) int {
+	value, ok := os.LookupEnv(key)
+	if !ok {
+		return fallback
+	}
+
+	// convert string to int using strconv.Atoi(ASCII to integer), it's a built-in function in go
+	intValue, err := strconv.Atoi(value)
+
+	if err != nil {
+		fmt.Printf("Error converting %s to int: %v\n", value, err)
+		return fallback
+	}
+	return intValue
+
+}
+
+func GetBool(key string, fallback bool) bool {
+	value, ok := os.LookupEnv(key)
+	if !ok {
+		return fallback
+	}
+
+	boolValue, err := strconv.ParseBool(value)
+	if err != nil {
+		fmt.Printf("Error converting %s to bool: %v\n", value, err)
+		return fallback
+	}
+	return boolValue
 }
--- ApiGateway/AuthInGo/main.go
+++ ApiGateway/AuthInGo/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"AuthInGo/app"
+	config "AuthInGo/config/env"
 )
 
 func main() {
+
+	config.Load() // Load environment variables from .env file
 
 	// creating a config object
 	cfg := app.NewConfig(":3005")
