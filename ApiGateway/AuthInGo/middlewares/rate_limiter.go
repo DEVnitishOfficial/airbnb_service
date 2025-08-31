c9c125f239d09bd4589a6d9f68a1585bd3b58eac
DEVnitishOfficial <nitish.naroun123@gmail.com>
2025-09-01 03:35:37 +0530

    added rate limiting based on ip address

--- ApiGateway/AuthInGo/middlewares/rate_limiter.go
+++ ApiGateway/AuthInGo/middlewares/rate_limiter.go
@@ -1,15 +1,52 @@
 package middlewares
 
 import (
+	"net"
 	"net/http"
+	"sync"
 	"time"
 
 	"golang.org/x/time/rate"
 )
 
-var limiter = rate.NewLimiter(rate.Every(1*time.Minute), 5) // 5 requests per minute
+// Store IP -> limiter
+var visitors = make(map[string]*rate.Limiter)
+var mu sync.Mutex
+
+// GetLimiter returns limiter for IP, or creates one
+func getLimiter(ip string) *rate.Limiter {
+	mu.Lock()
+	defer mu.Unlock()
+
+	limiter, exists := visitors[ip]
+	if !exists {
+		// 5 requests per minute per IP
+		limiter = rate.NewLimiter(rate.Every(time.Minute), 5)
+		visitors[ip] = limiter
+	}
+	return limiter
+}
+
+// Middleware
 func RateLimitMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			http.Error(w, "Unable to determine IP", http.StatusInternalServerError)
+			return
+		}
+
+		// to check for differnet ip address uncomment the below code and comment the above code
+		/** in postman to simulate different ip address we can use the X-Forwarded-For header
+		in post man put X-Forwarded-For : 127.0.0.1
+
+		ip := r.Header.Get("X-Forwarded-For")
+		if ip == "" {
+			ip, _, _ = net.SplitHostPort(r.RemoteAddr)
+		}
+		*/
+
+		limiter := getLimiter(ip)
 		if !limiter.Allow() {
 			http.Error(w, "Too many requests", http.StatusTooManyRequests)
 			return
